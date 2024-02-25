b1bfacd8e403d4a91613e385db664ae0953c477d
mohajiabdi <135143797+mohajiabdi@users.noreply.github.com>
2024-02-25 11:47:24 +0300

    a
    a

--- Variables/src/BuyingCar.java
+++ Variables/src/BuyingCar.java
@@ -8,14 +8,17 @@ public class BuyingCar {
         final int TAX = 65;
         double carPrice;
         String carName, carModel;
-        Scanner input = new Scanner(System.in);
 
+        // Create Object from Scanner Calss
+        Scanner input = new Scanner(System.in);
         System.out.printf("Enter Car Price: ");
         carPrice = input.nextDouble();
         System.out.printf("Enter Car NAme: ");
         carName = input.next();
         System.out.printf("Enter Car Model: ");
         carModel = input.next();
+        
+        // Calculating Total And Tax
         double total = carPrice + TAX;
 
         System.out.printf("The Car of "+carName+" With the Model: "+ carModel+" And its Price after tax is: "+total);
