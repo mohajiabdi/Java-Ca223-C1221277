9408a6a0fb40f247d96a414a2a222802f1cc5199
mohajiabdi <135143797+mohajiabdi@users.noreply.github.com>
2024-02-25 10:09:25 +0300

    new Update
    And this is done

--- Double The Number/src/Main.java
+++ Double The Number/src/Main.java
@@ -7,13 +7,16 @@ public class Main {
         int a = 35;
         int b = 30;
         int c = a + b;
-
         System.out.println("The Value of "+a+" + "+b +" is : " +c);
 
-        System.out.println("Enter Any Number: ");
         Scanner inputNumber = new Scanner(System.in);
-        double number = inputNumber.nextDouble();
-        System.out.println("The Double Of The Number is: " + number*2);
+        System.out.println("Enter Three Different Numbers: ");
+        double number1 = inputNumber.nextDouble();
+        int number2 = inputNumber.nextInt();
+        long number3 = inputNumber.nextLong();
+        double total = number1 + number2 *number3;
+        System.out.println("The Total Number is: " +total );
+        System.out.println("And This is The end!!!");
 
 
     }
