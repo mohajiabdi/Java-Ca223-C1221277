044f39b8c83a24ee0f2ecf1e35ee1036a8d3878f
mohajiabdi <135143797+mohajiabdi@users.noreply.github.com>
2024-05-05 17:53:58 +0300

    more

---
+++ Loops/src/LoopInLoop.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class LoopInLoop {
+    public static void main(String[] args) {
+        int count = 3;
+
+        int pass = 1234;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Your Password: ");
+        int pin  = input.nextInt();
+
+
+        while (pin != pass && count >0){
+            System.out.printf("\nIncorrect Password.\n" +
+                    "You have %d Times left To Try Again!" +
+                    "\nEnter Your Password: ",count);
+            pin  = input.nextInt();
+            count--;
+        }
+
+        if(pin == pass){
+            System.out.println("Correct Password!");
+        }else{
+            System.out.println("You Have Entered The Wrong Password 3 Times");
+        }
+    }
+}
