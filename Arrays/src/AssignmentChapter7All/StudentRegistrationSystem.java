c3d1054ace392f221d7b89fe02bb3d1e2fe457fc
mohajiabdi <135143797+mohajiabdi@users.noreply.github.com>
2024-07-12 22:54:59 +0300

    ok

--- Arrays/src/AssignmentChapter7All/StudentRegistrationSystem.java
+++ Arrays/src/AssignmentChapter7All/StudentRegistrationSystem.java
@@ -8,39 +8,34 @@ public class StudentRegistrationSystem {
         int[] studentIds = new int[5];
         String[] studentNames = new String[studentIds.length];
 
+        for (int i = 0; i < studentIds.length; i++) {
+            System.out.printf("Student Id %d: ", i);
+            int id = input.nextInt();
 
-
-//       for(int i=0; i<studentIds.length; i++) {
-//            System.out.println();
-//            System.out.printf("Student Id %d: ",i);
-//            studentIds[i] = input.nextInt();
-//        }
-
-        for (int i = 0; i < studentIds.length; i++){
-            int id= input.nextInt();
-            for(int j=0; j<studentIds.length; j++){
-                if(studentIds[j] == id){
-                    System.out.printf("Student Id %d: ",i);
-                    studentIds[i] = input.nextInt();
-                }else {
-                    studentIds[i]=id;
+            // Check for duplicate ID
+            boolean isDuplicate = false;
+            for (int j = 0; j < i; j++) {
+                if (studentIds[j] == id) {
+                    isDuplicate = true;
+                    break;
                 }
             }
-        }
 
-        for(int i = 0; i <studentNames.length; i++) {
-            System.out.println();
-            System.out.printf("Student name %d: ", studentIds[i]);
-            studentNames[i] = input.next();
-
-        }
-        for (int i = 0; i < studentIds.length; i++){
-
-            for(String name : studentNames){
-                System.out.println("Student id: "+studentIds[i]+" and Student name: "+name);
+            if (isDuplicate) {
+                System.out.println("This ID already exists. Please enter a different ID.");
+                i--; // Decrement i to re-prompt for the current index
+            } else {
+                studentIds[i] = id;
             }
         }
 
+        for (int i = 0; i < studentNames.length; i++) {
+            System.out.printf("Student name with id %d: ", studentIds[i]);
+            studentNames[i] = input.next();
+        }
 
-        }}
-
+        for (int i = 0; i < studentIds.length; i++) {
+            System.out.println("Student id: " + studentIds[i] + " and Student name: " + studentNames[i]);
+        }
+    }
+}
