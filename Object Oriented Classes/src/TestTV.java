6fafa3e8e73e4cc62a1aac46208efc847f03088a
mohajiabdi <135143797+mohajiabdi@users.noreply.github.com>
2024-07-13 05:38:22 +0300

    ok

--- Object Oriented Classes/src/TestTV.java
+++ Object Oriented Classes/src/TestTV.java
@@ -10,6 +10,9 @@ public class TestTV {
         tv2.channelUp();
         tv2.channelUp();
         tv2.volumeUp(); // Increase tv2 volume up 1 level
+       // tv2.volumeDown();
+        tv2.volumeUp();
+        tv2.volumeUp();
 
         System.out.println("tv1's channel is " + tv1.channel
                 + " and volume level is " + tv1.volumeLevel);
